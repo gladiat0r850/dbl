a0711833a8413f362d11d8fe18ce8d698cc3262f
X3NOGLADIAT0R <game23452332pro@gmail.com>
2024-11-24 11:52:20 +0400

    tsc

--- ndm/app.ts
+++ ndm/app.ts
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-import { Response, Request } from "express"
+import { Request, Response } from "express"
 
 interface Legends {
     name: string
