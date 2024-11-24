f9e2610ec1032911e859f436b06ce8c12d27dbe7
X3NOGLADIAT0R <game23452332pro@gmail.com>
2024-11-24 20:27:08 +0400

    nothing

--- ndm/app.js
+++ ndm/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require('cors')
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}))
 
 const team = [
     {
